ac92193e33f16e0e433ceb451c3cae34e69a8af6
Ernesto B <ernestobravc@gmail.com>
2024-10-11 18:28:34 -0600

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+import os
+from typing import List
+
+def get_requirements(file_path:str) -> List:
+    req_path=os.path.join(os.getcwd(), file_path)
+
+    with open(req_path, 'r') as file:
+        requirements=file.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+
+    return requirements
+
+
+
+setup(
+    name='machine_learning_project',
+    version='0.0.1',
+    author='ErnstB',
+    author_email='ernestobravc@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+)
